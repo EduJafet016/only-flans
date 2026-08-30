e9eb1db4bf56767851f3fb7b6ce7130197193aa3
EduJafet016 <edu.yafet123321@gmail.com>
2026-08-29 18:52:13 -0600

    correccion

--- src/main/java/com/example/onlyflans/dto/OrdenRequest.java
+++ src/main/java/com/example/onlyflans/dto/OrdenRequest.java
@@ -1,20 +1,8 @@
 package com.example.onlyflans.dto;
 
-import java.time.LocalDate;
-
-public class OrdenRequest {
-    private String telefono;
-    private String nombre;
-    private int cantidad;
-    private LocalDate fechaDeseada;
-
-    // Genera los Getters y Setters con IntelliJ (Alt + Insert)
-    public String getTelefono() { return telefono; }
-    public void setTelefono(String telefono) { this.telefono = telefono; }
-    public String getNombre() { return nombre; }
-    public void setNombre(String nombre) { this.nombre = nombre; }
-    public int getCantidad() { return cantidad; }
-    public void setCantidad(int cantidad) { this.cantidad = cantidad; }
-    public LocalDate getFechaDeseada() { return fechaDeseada; }
-    public void setFechaDeseada(LocalDate fechaDeseada) { this.fechaDeseada = fechaDeseada; }
-}
+public record OrdenRequest(
+        String telefono,
+        String nombre,
+        int cantidad,
+        String fechaDeseada
+) {}
